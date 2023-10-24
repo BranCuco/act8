8fc757e55daf6be49793e2ea7dd751f15fc882d3
BranCuco <bran.escuela@gmail.com>
2023-10-23 18:47:11 -0600

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace act8
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.labelNum1 = new System.Windows.Forms.Label();
+            this.labelNum2 = new System.Windows.Forms.Label();
+            this.textBoxNum1 = new System.Windows.Forms.TextBox();
+            this.textBoxNum2 = new System.Windows.Forms.TextBox();
+            this.buttonSumar = new System.Windows.Forms.Button();
+            this.labelResultado = new System.Windows.Forms.Label();
+            this.textBoxResultado = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // labelNum1
+            // 
+            this.labelNum1.AutoSize = true;
+            this.labelNum1.Location = new System.Drawing.Point(12, 15);
+            this.labelNum1.Name = "labelNum1";
+            this.labelNum1.Size = new System.Drawing.Size(51, 13);
+            this.labelNum1.TabIndex = 0;
+            this.labelNum1.Text = "Número 1";
+            // 
+            // labelNum2
+            // 
+            this.labelNum2.AutoSize = true;
+            this.labelNum2.Location = new System.Drawing.Point(12, 41);
+            this.labelNum2.Name = "labelNum2";
+            this.labelNum2.Size = new System.Drawing.Size(51, 13);
+            this.labelNum2.TabIndex = 1;
+            this.labelNum2.Text = "Número 2";
+            // 
+            // textBoxNum1
+            // 
+            this.textBoxNum1.Location = new System.Drawing.Point(69, 12);
+            this.textBoxNum1.Name = "textBoxNum1";
+            this.textBoxNum1.Size = new System.Drawing.Size(100, 20);
+            this.textBoxNum1.TabIndex = 2;
+            // 
+            // textBoxNum2
+            // 
+            this.textBoxNum2.Location = new System.Drawing.Point(69, 38);
+            this.textBoxNum2.Name = "textBoxNum2";
+            this.textBoxNum2.Size = new System.Drawing.Size(100, 20);
+            this.textBoxNum2.TabIndex = 3;
+            // 
+            // buttonSumar
+            // 
+            this.buttonSumar.Location = new System.Drawing.Point(15, 64);
+            this.buttonSumar.Name = "buttonSumar";
+            this.buttonSumar.Size = new System.Drawing.Size(154, 23);
+            this.buttonSumar.TabIndex = 4;
+            this.buttonSumar.Text = "Sumar";
+            this.buttonSumar.UseVisualStyleBackColor = true;
+            this.buttonSumar.Click += new System.EventHandler(this.buttonSumar_Click);
+            // 
+            // labelResultado
+            // 
+            this.labelResultado.AutoSize = true;
+            this.labelResultado.Location = new System.Drawing.Point(12, 93);
+            this.labelResultado.Name = "labelResultado";
+            this.labelResultado.Size = new System.Drawing.Size(58, 13);
+            this.labelResultado.TabIndex = 5;
+            this.labelResultado.Text = "Resultado:";
+            // 
+            // textBoxResultado
+            // 
+            this.textBoxResultado.Location = new System.Drawing.Point(76, 90);
+            this.textBoxResultado.Name = "textBoxResultado";
+            this.textBoxResultado.ReadOnly = true;
+            this.textBoxResultado.Size = new System.Drawing.Size(97, 20);
+            this.textBoxResultado.TabIndex = 6;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(181, 124);
+            this.Controls.Add(this.textBoxResultado);
+            this.Controls.Add(this.labelResultado);
+            this.Controls.Add(this.buttonSumar);
+            this.Controls.Add(this.textBoxNum2);
+            this.Controls.Add(this.textBoxNum1);
+            this.Controls.Add(this.labelNum2);
+            this.Controls.Add(this.labelNum1);
+            this.Name = "MainForm";
+            this.Text = "Calculadora";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Label labelNum1;
+        private System.Windows.Forms.Label labelNum2;
+        private System.Windows.Forms.TextBox textBoxNum1;
+        private System.Windows.Forms.TextBox textBoxNum2;
+        private System.Windows.Forms.Button buttonSumar;
+        private System.Windows.Forms.Label labelResultado;
+        private System.Windows.Forms.TextBox textBoxResultado;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Windows.Forms;
+namespace act8
+{
+    public partial class Form1 : Form
+    {
+        public Stack<int> SumarStack(string num1, string num2)
+        {
+            Stack<int> Num1 = new Stack<int>();
+            Stack<int> Num2 = new Stack<int>();
+            Stack<int> resultado = new Stack<int>();
+
+            foreach (char digito in num1)
+            {
+                Num1.Push(int.Parse(digito.ToString()));
+            }
+
+            foreach (char digito in num2)
+            {
+                Num2.Push(int.Parse(digito.ToString()));
+            }
+
+            int acarreo = 0;
+            int digito1 = 0;
+            int digito2 = 0;
+            while (Num1.Count > 0 || Num2.Count > 0)
+            {
+                if (Num1.Count == 0)
+                {
+                    digito1 = 0;
+                }
+                else
+                {
+                    digito1 = Num1.Pop();
+                }
+
+                if (Num2.Count == 0)
+                {
+                    digito2 = 0;
+                }
+                else
+                {
+                    digito2 = Num2.Pop();
+                }
+
+                int res = digito1 + digito2 + acarreo;
+
+                if (res >= 10)
+                {
+                    acarreo = res / 10;
+                    res %= 10;
+                }
+                else
+                {
+                    acarreo = 0;
+                }
+
+                resultado.Push(res);
+
+                if (Num1.Count == 0 && Num2.Count == 0 && acarreo != 0)
+                {
+                    resultado.Push(acarreo);
+                }
+            }
+            return resultado;
+        }
+        private void buttonSumar_Click(object sender, EventArgs e)
+        {
+            string num1 = textBoxNum1.Text;
+            string num2 = textBoxNum2.Text;
+
+            Form1 form = new Form1();
+            Stack<int> resultado = form.SumarStack(num1, num2);
+
+            textBoxResultado.Text = string.Join("", resultado);
+        }
+    }
+}
